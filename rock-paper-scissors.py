830e2f1ffeb11569af35c6b964ae241c620a7b8f
WillChomrak <wchomrak@gmail.com>
2021-09-16 07:25:41 -0500

    add player input functionality

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,3 +1,18 @@
 # want to make a simple app that randomly chooses rock, paper, or scissors to battle me
 # later functionalities to add could be: score tracker
 
+# so what do we need here?
+# a func to take an input
+# a func to randomly choose a r/p/s
+# a func to compare both and return the winner
+
+def playerChoice():
+    selection = input("Please enter 'rock', 'paper', or 'scissors': ").lower()
+    
+    if selection == 'rock' or selection == 'paper' or selection == 'scissors':
+        return selection
+    else:
+        print('Input not recognized. Please enter one of the options exactly.')
+        return playerChoice()
+
+     
