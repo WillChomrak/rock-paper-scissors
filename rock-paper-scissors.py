e6297d1403d47bc8767e7b61738cf33a2752db3a
WillChomrak <wchomrak@gmail.com>
2021-09-16 07:40:14 -0500

    add computer fighter selection function; add list of fighters; update player fighter selection to use list of fighters

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -6,13 +6,31 @@
 # a func to randomly choose a r/p/s
 # a func to compare both and return the winner
 
+import random
+
+fighters = ['rock', 'paper', 'scissors']
+
+# func to take request and take input from user 
+# and to check if user has input one of 3 options
 def playerChoice():
+    # takes input and converts to all lowercase for simplicity
     selection = input("Please enter 'rock', 'paper', or 'scissors': ").lower()
     
-    if selection == 'rock' or selection == 'paper' or selection == 'scissors':
+    # check user input an appropriate choice
+    if selection in fighters:
         return selection
     else:
         print('Input not recognized. Please enter one of the options exactly.')
         return playerChoice()
+
+# func to randomly select computer fighter
+def compChoice():
+    # generate random number between 0 and 2 INCLUSIVE
+    randNum = random.randint(0, 2)
+    # pick fighter using randNum
+    selection = fighters[randNum]
+    return selection
+
+
 
      
